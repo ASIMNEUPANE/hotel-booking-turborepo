7bf2f58a6474ee72567fb51ecdf61638f94d6756
ASIMNEUPANE <asimneupane11@gmail.com>
2024-07-20 13:00:40 +0545

    newfix

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthsModule } from './auths/auths.module';
+import { UserModule } from './users/user.module';
+import { PrismaService } from './prisma/prisma.service';
+import { RoleGuard } from './auths/guards/role.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthsModule, UserModule],
+  controllers: [],
+  providers: [{ provide: APP_GUARD, useClass: RoleGuard }, PrismaService],
 })
 export class AppModule {}
